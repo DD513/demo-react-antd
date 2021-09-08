b2315c87a93d7654564eec53f8ebcdea7b8b5f55
DD <50895419+DD513@users.noreply.github.com>
2021-09-08 16:09:28 +0800

    demo2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb , Button } from 'antd';
+import { SearchOutlined , DownloadOutlined } from '@ant-design/icons';
 import './App.css';
+const { Header, Footer, Content } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        General
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        Layout
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+        Navigation
+      </a>
+    </Menu.Item>
+  </Menu>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Ant Design</Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <a href="">Component</a>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item overlay={menu}>
+              <a href="">General</a>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>Button</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+            <Button icon={<SearchOutlined />}>Search</Button>
+            <Button type="primary" icon={<DownloadOutlined />}/>
+            <Button danger>Danger Default</Button>
+
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    );
+  }
 }
 
 export default App;
